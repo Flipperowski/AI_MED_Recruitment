de8ee6acb16ff69a207d9e699f81a58215d84790
Flipperowski <68556727+Flipperowski@users.noreply.github.com>
2025-10-14 21:57:31 +0200

    3rd Fork
    - Tuned KNN
    - Changed Notebook

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, RepeatedStratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
@@ -16,7 +16,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
 # Ewaluation
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, auc
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, auc, f1_score, roc_auc_score
 
 #Importing the data from CSV (80% training, 20% testing)
 data = pd.read_csv("task_data.csv")
@@ -51,20 +51,53 @@ scaler = StandardScaler()
 X_scaled_train = scaler.fit_transform(X_train)
 X_scaled_test = scaler.transform(X_test)
 
+#Defining hyperparameters
+param_grid = {
+    "model__n_neighbors": [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+    "model__weights": ["uniform", "distance"],
+    "model__metric": ["minkowski", "manhattan", "euclidean"],
+    "model__p": [1, 2]
+}
+
+#Setting up the cross-validation strategy
+rskf = RepeatedStratifiedKFold(
+    n_splits=5,
+    n_repeats=5,
+    random_state=42
+)
+
 #Applying K-Nearest Neighbors (KNN) Classifier
 pipe_knn = Pipeline(steps=[
     ("scaler", StandardScaler()),
-    ("model", KNeighborsClassifier(
-        n_neighbors = 3,
-        weights='distance',
-        metric='manhattan'
-    ))
+    ("model", KNeighborsClassifier())
 ])
 
-pipe_knn.fit(X_train, y_train)
-cv_score = np.round(cross_val_score(pipe_knn, X_train, y_train), 2)
+#Initializing the Grid Search for the KNN model
+grid_search = GridSearchCV(
+    estimator=pipe_knn,
+    param_grid=param_grid,
+    scoring="accuracy",
+    cv=rskf,
+    verbose=0,
+    n_jobs=-1
+)
+
+#Training
+grid_search.fit(X_train, y_train)
 
 #Displaying results
+print("General test --- KNN Model \n")
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best accuracy (averaged CV): {grid_search.best_score_:.4f}")
+
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+print("Test accuracy:", accuracy_score(y_test, y_pred))
+print("F1:", f1_score(y_test, y_pred))
+print("ROC AUC:", roc_auc_score(y_test, y_pred))
+
+print("Test of each fold\n")
+cv_score = np.round(cross_val_score(pipe_knn, X_train, y_train), 2)
 print("Scores of training data cross-validation (each fold):")
 list(map(print, cv_score))
 print(f"\nCross-validation mean score: {np.mean(cv_score):.3}")
