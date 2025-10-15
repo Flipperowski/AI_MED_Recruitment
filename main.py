c19603821dd1ddbe354f7b3ec4ca9317277e8bf7
Flipperowski <68556727+Flipperowski@users.noreply.github.com>
2025-10-15 16:21:36 +0200

    Add files via upload
    Added Decision Tree and optimized accuracy test

--- main.py
+++ main.py
@@ -15,8 +15,26 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
-# Ewaluation
+# Metrics
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, auc, f1_score, roc_auc_score
+
+
+#Evaluation function
+def evaluate_model(model, X, y, cv=5):
+    # Accuracy
+    acc = cross_val_score(model, X, y, cv=cv, scoring="accuracy")
+    print("\nAccuracy scores:", np.round(acc, 2))
+    print(f"Mean Accuracy: {np.mean(acc):.3f} | Std: {np.std(acc):.3f}")
+
+    # F1
+    f1 = cross_val_score(model, X, y, cv=cv, scoring="f1")
+    print("\nF1 scores:", np.round(f1, 2))
+    print(f"Mean F1: {np.mean(f1):.3f} | Std: {np.std(f1):.3f}")
+
+    # ROC AUC
+    auc = cross_val_score(model, X, y, cv=cv, scoring="roc_auc")
+    print("\nROC AUC scores:", np.round(auc, 2))
+    print(f"Mean ROC AUC: {np.mean(auc):.3f} | Std: {np.std(auc):.3f}")
 
 #Importing the data from CSV (80% training, 20% testing)
 data = pd.read_csv("task_data.csv")
@@ -84,21 +102,36 @@ grid_search = GridSearchCV(
 
 #Training
 grid_search.fit(X_train, y_train)
+best_knn = grid_search.best_estimator_
 
 #Displaying results
 print("General test --- KNN Model \n")
 print(f"Best parameters: {grid_search.best_params_}")
-print(f"Best accuracy (averaged CV): {grid_search.best_score_:.4f}")
+print(f"Best accuracy (averaged CV): {grid_search.best_score_:.4f}\n\n")
 
-best_model = grid_search.best_estimator_
-y_pred = best_model.predict(X_test)
-print("Test accuracy:", accuracy_score(y_test, y_pred))
-print("F1:", f1_score(y_test, y_pred))
-print("ROC AUC:", roc_auc_score(y_test, y_pred))
+evaluate_model(best_knn, X_train, y_train)
 
-print("Test of each fold\n")
-cv_score = np.round(cross_val_score(pipe_knn, X_train, y_train), 2)
-print("Scores of training data cross-validation (each fold):")
+#Decision Tree
+clf_tree = DecisionTreeClassifier(
+    max_depth=5,
+    criterion='entropy',
+    min_samples_split=8,
+    min_samples_leaf=8,
+    class_weight=None,
+    random_state=42
+)
+
+#Training
+clf_tree.fit(X_train, y_train)
+
+cv_score = np.round(cross_val_score(clf_tree, X_train, y_train),2 )
+
+#Displaying results
+print("\n\nGeneral test --- Decision Tree \n")
+
+print(f"Scores of training data cross-validation (each fold):")
 list(map(print, cv_score))
 print(f"\nCross-validation mean score: {np.mean(cv_score):.3}")
-print(f"Standard deviation of CV score: {np.std(cv_score):.3f}")
+print(f"Standard deviation of CV score: {np.std(cv_score):.3f}\n\n")
+
+evaluate_model(clf_tree, X_train, y_train)
